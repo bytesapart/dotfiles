11b0e6c1e3eb5a43bf1e42280e468cc5f92591fa
Osama Iqbal <iqbal.osama@icloud.com>
2022-06-07 00:39:00 +0530

    feat: Add the initial stub for arch linux post install script

---
+++ arch_post_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script installs some of the post-installation scripts that I need
+# the objective of this script would be, in it's final state, to
+# a. Install all the pacman softwares I need
+# b. Copy all the configurations that I would need.
+# Therefore, this script will be a post installation script that will set up a lot of environment for me
+# after the `archinstall` has been run once the arch iso boots up
+

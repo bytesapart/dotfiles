7c5a3d13bb6808efde744086fb6f0c6915ab32dc
Osama Iqbal <iqbal.osama@icloud.com>
2022-07-01 01:17:45 +0530

    feat: Add powertop to install

--- scripts/arch_post_install.sh
+++ scripts/arch_post_install.sh
@@ -37,7 +37,7 @@ sudo pacman -S ebtables libguestfs dmidecode --noconfirm
 sudo pacman -S ncurses libevent libutempter wget curl --noconfirm
 sudo pacman -S scrcpy xf86-input-wacom --noconfirm
 sudo pacman -S usbutils kdiskmark --noconfirm
-sudo pacman -S rustup go obsidian upower --noconfirm
+sudo pacman -S rustup go obsidian upower powertop --noconfirm
 sudo pacman -S udisks2 --noconfirm
 sudo pacman -S zsh arandr bluez bluez-utils nodejs npm --noconfirm
 sudo pacman -S cronie gparted swtpm --noconfirm

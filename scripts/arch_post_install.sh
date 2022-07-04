2cb4f28b1c03fa700e7f13f5bd6bd7ee15dc9bac
Osama Iqbal <iqbal.osama@icloud.com>
2022-07-04 20:20:05 +0530

    fix: Fix zathurarc file

--- scripts/arch_post_install.sh
+++ scripts/arch_post_install.sh
@@ -219,7 +219,7 @@ chmod 644 .config/i3/config
 
 # Copy zathura configurations
 mkdir .config/zathura
-cp ~/Project/dotfiles/zathura/.zathurarc .config/zathura/.zathurarc
+cp ~/Project/dotfiles/zathura/zathurarc .config/zathura/zathurarc
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
